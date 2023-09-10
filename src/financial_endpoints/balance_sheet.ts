93137c9c6cdc82c14924c0ef6d11f9a0d9710f38
Richard-Otterli <99365919+Richard-Otterli@users.noreply.github.com>
2023-09-10 10:59:25 +0100

    Financial client cleanup

--- src/financial_endpoints/balance_sheet.ts
+++ src/financial_endpoints/balance_sheet.ts
@@ -1,16 +1,10 @@
-import { APIClient } from '../utils/client.js';
-
-const host = 'https://fmpcloud.io/api/v3';
-const apiKey = '48c4ab01b637a5eb0c8d133189d36f30';
+import { APIClient, getConfig } from '../utils/client.js';
 
 
 export class BalanceSheet extends APIClient {
 
     public constructor(ticker: string){
         super();
-        this.api_url = `${host}/balance-sheet-statement/${ticker}?apikey=${apiKey}`;
+        this.api_url = `${getConfig().API_HOST}/balance-sheet-statement/${ticker}?apikey=${getConfig().API_KEY}`;
     }
 }
-const apple_income = new BalanceSheet('AAPL');
-const apple_data = await apple_income.getJson();
-console.log(apple_data);
